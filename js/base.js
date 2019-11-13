21e997e16d942e46371a5dc030d969a887db73d7
krisfant <404832261@qq.com>
2019-11-13 18:36:48 +0800

    二维码更新

--- js/base.js
+++ js/base.js
@@ -2,5 +2,12 @@
  * Created by baizhantang on 2019/11/13.
  */
 function showWX() {
-    document.getElementById("wx").style.display = 'block';
+
+    var e = document.getElementById("wx");
+    if(e.style.display == 'block'){
+        document.getElementById("wx").style.display = 'none';
+    }
+    else {
+        document.getElementById("wx").style.display = 'block';
+    };
 }
